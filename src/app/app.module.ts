fb5ddc3c9d54609e9cee702b54af0bd17c5136e0
Karla Falcon <karla1dfalcocon@gmail.com>
2023-09-22 21:10:31 -0600

    --22/09/2023 Karla Falcon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import{FormsModule} from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { ProductoListComponent } from './productos/producto-list/producto-list.component';
+import { ProductoFilterPipe } from './productos/producto-filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductoListComponent,
+    ProductoFilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
